1e751c105376ffb2ff27b25ed522b33eb8cc1cfa
Karan <email@karanshergill.com>
2024-07-16 17:16:09 +0530

    complete

---
+++ tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'Figtree': ["Figtree", "sans-serif"]
+            },
+            colors: {
+                'Primary-Yellow': 'hsl(47, 88%, 63%)',
+                'Neutral-White': 'hsl(0, 0%, 100%)',
+                'Neutral-Grey': 'hsl(0, 0%, 50%)',
+                'Neutral-Black': 'hsl(0, 0%, 7%)'
+            },
+            boxShadow: {
+                'custom': '10px 10px 0px -2px rgba(0, 0, 0, 1)'}
+
+        }
+    }
+}
